1afef07044ecd0e8a1d670aab2d16ec792a84311
H Dub <hwilson@rapid7.com>
2023-08-17 17:58:40 -0500

    Choose to agree and disagree with shellcheck

--- src/_docker_manager.sh
+++ src/_docker_manager.sh
@@ -7,9 +7,10 @@ postgresComposeService="postgres"
 function compose_client() {
   flags=($@)
   compose_files=$(for i in `ls -r ${PLEXTRAC_HOME}/docker-compose*.yml`; do printf " -f %s" "$i"; done )
-  debug "docker compose flags: ${flags[@]}"
+  debug "docker compose flags: ${flags[*]}"
   debug "docker compose configs: ${compose_files}"
-  docker compose $(echo $compose_files) ${flags[@]}
+  # shellcheck disable=SC2086,SC2068
+  docker compose $compose_files ${flags[@]}
 }
 
 function pull_docker_images() {
@@ -25,18 +26,17 @@ function pull_docker_images() {
 
 function composeConfigNeedsUpdated() {
   info "Checking for pending changes to docker-compose.yml"
-  decodedComposeFile=$(base64 -d <<<$DOCKER_COMPOSE_ENCODED)
+  decodedComposeFile=$(base64 -d <<<"$DOCKER_COMPOSE_ENCODED")
   targetComposeFile="${PLEXTRAC_HOME}/docker-compose.yml"
-  if [ $(echo "$decodedComposeFile" | md5sum | awk '{print $1}') == $(md5sum $targetComposeFile | awk '{print $1}') ]; then
+  if [ "$(echo "$decodedComposeFile" | md5sum | awk '{print $1}')" == "$(md5sum "$targetComposeFile" | awk '{print $1}')" ]; then
     debug "docker-compose.yml content matches"; return 1;
   fi
-  diff -N --unified=2 --color=always --label existing --label "updated" $targetComposeFile <(echo "$decodedComposeFile") || return 0
+  diff -N --unified=2 --color=always --label existing --label "updated" "$targetComposeFile" <(echo "$decodedComposeFile") || return 0
   return 1
 }
 
 function docker_createInitialComposeOverrideFile() {
   local targetOverrideFile="${PLEXTRAC_HOME}/docker-compose.override.yml"
-
   info "Checking for existing $targetOverrideFile"
   if ! test -f "$targetOverrideFile"; then
     info "Creating initial $targetOverrideFile"
